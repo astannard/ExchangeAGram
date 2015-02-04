25e10e7ff8507f8945caef313543b608ab8e4f24
Andy Stannard <andystannawrd@me.com>
2015-02-04 08:40:16 +0000

    added and setup Facebook SDK

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Andy Stannard on 04/02/2015.
+//  Copyright (c) 2015 inni Accounts. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
